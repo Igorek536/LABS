4239f0327f748f072d035641caf493dc60e8785d
Igorek536 <igorek536@MYTHINKPAD.MYDOMAIN>
2018-12-14 00:33:25 +0200

    Initial commit

---
+++ app/src/main/java/com/gangster/labs/utils/StringUtils.java
@@ -0,0 +1,12 @@
+package com.gangster.labs.utils;
+
+public class StringUtils {
+    public static boolean isInteger(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
